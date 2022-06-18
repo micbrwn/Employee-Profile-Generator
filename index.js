6589c19e04d77b8085567a841b732763b3bfe6ba
Michael <Michael@Ms-MBA.local>
2022-06-18 15:05:23 -0400

    completed code for index.js and template.js

--- index.js
+++ index.js
@@ -1,95 +1,55 @@
+const Employee = require('./lib/Employee')
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const inquirer = require('inquirer');
 const path = require('path');
 const fs = require('fs');
-const {renderHTML} = require('./src/template');
-// Imports page template function and assign the function to a variable
-// Create variables for the output folder and the html file name
+const { generateHTML } = require('./src/template');
 
-const team = [];
-const employeeID = [];
+let team = [];
 
-const questions = {
-    Manager: [
+
+const minions = () => {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            message: 'what is the employees role?',
+            choices: ['Manager', 'Engineer', 'Intern'],
+            name: 'role'
+        },
         {
             type: "input",
-            message: "What is the name of the manager?",
+            message: "What is the name of the empoyee?",
             name: "name"
         },
         {
             type: "input",
-            message: "What is the manager's Id?",
+            message: "What is the employee's Id?",
             name: "id"
         },
         {
             type: "input",
-            message: "What is the manager's email?",
+            message: "What is the employee's email?",
             name: "email"
         },
         {
             type: "input",
             message: "What is the manager's office number?",
-            name: "officeNumber"
-        },
-        {
-            type: "list",
-            message: "Do you want to add another employee?",
-            choices: ["yes", "no"],
-            name: "add"
-        }
-    ],
-
-    Engineer: [
-        {
-            type: "input",
-            message: "What is the name of the engineer?",
-            name: "name"
-        },
-        {
-            type: "input",
-            message: "What is the engineer's Id?",
-            name: "id"
-        },
-        {
-            type: "input",
-            message: "What is the engineer's email?",
-            name: "email"
+            name: "officeNumber",
+            when: (input) => input.role === 'Manager'
         },
         {
             type: "input",
             message: "What is the engineer's GitHub username?",
-            name: "github"
-        },
-        {
-            type: "list",
-            message: "Do you want to add another employee?",
-            choices: ["yes", "no"],
-            name: "add"
-        }
-    ],
-
-    Intern: [
-        {
-            type: "input",
-            message: "What is the name of the intern?",
-            name: "name"
-        },
-        {
-            type: "input",
-            message: "What is the Intern's Id?",
-            name: "id"
-        },
-        {
-            type: "input",
-            message: "What is the Intern's email?",
-            name: "email"
+            name: "github",
+            when: (input) => input.role === 'Engineer'
         },
         {
             type: "input",
             message: "What is the school of the Intern?",
-            name: "school"
+            name: "school",
+            when: (input) => input.role === 'Intern'
         },
         {
             type: "list",
@@ -97,5 +57,30 @@ const questions = {
             choices: ["yes", "no"],
             name: "add"
         }
-    ]
-};
+    ])
+        .then(response => {
+            if (response.role === 'Manager') {
+                const { name, id, email, officeNumber } = response;
+                const manager = new Manager(name, id, email, officeNumber);
+                team.push(manager);
+            }
+
+            else if (response.role === 'Engineer') {
+                const { name, id, email, github } = response;
+                const engineer = new Engineer(name, id, email, github);
+                team.push(engineer);
+            }
+            else if (response.role === 'Intern') {
+                const { name, id, email, school } = response;
+                const intern = new Intern(name, id, email, school);
+                team.push(intern);
+            }
+
+            (response.add === 'yes') ? minions() : generateHTML(team);
+
+        })
+}
+
+minions();
+
+// module.exports = index;
--- src/template.js
+++ src/template.js
@@ -1,21 +1,143 @@
-const { template } = require("@babel/core");
-const { moduleExpression } = require("@babel/types");
-
+// const { template } = require("@babel/core");
+// const { moduleExpression } = require("@babel/types");
+const index = require('../index');
+const fs = require('fs');
+// const indexjs = { index };
 /**********************************************************
   REMOVE ALL THE COMMENTS BEFORE SUBMITTING YOUR HOMEWORK
 ***********************************************************/
-function renderHTML() {
-    return `
-    <h1>Hello</h1>
+let htmlTemplate = [];
+// let managerTemplate = [];
+// let engineerTemplate = [];
+// let internTemplate = [];  
+
+const employeesTemplate = (team) => {
+  for (i = 0; i < team.length; i++) {
+    let role = team[i].getRole();
+    switch (role) {
+
+      case "Manager": 
+      generateManager(team[i])
+      break;
+
+      case "Intern":
+      generateIntern(team[i])
+      break;
+
+      case "Engineer":
+      generateEngineer(team[i])
+      break;
+    }
+  }
+
+  
+  function generateManager(manager) {
+    let managerCard = `
+        <div class="card employee-card" style="width: 18rem;">
+    <div class="card-header">
+        <h2 class="card-title">${manager.getName()}</h2>
+        <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
+    </div>
+    <div class="card-body">
+        <ul class="list-group">
+            <li class="list-group-item">ID: ${manager.getId()}</li>
+            <li class="list-group-item">Email: <a href="${manager.getEmail()}">${manager.getEmail()}</a></li>
+            <li class="list-group-item">Office number: ${manager.getOfficeNumber()}</li>
+        </ul>
+    </div>
+</div>
     `
+    htmlTemplate.push(managerCard)
+  }
+
+  function generateEngineer(engineer) {
+    let engineerCard = `
+    <div class="card employee-card" style="width: 18rem;">
+    <div class="card-header">
+        <h2 class="card-title">${engineer.getName()}</h2>
+        <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.getRole()}</h3>
+    </div>
+    <div class="card-body">
+        <ul class="list-group">
+            <li class="list-group-item">ID:${engineer.getId()}</li>
+            <li class="list-group-item">Email: <a href="${engineer.getEmail()}">${engineer.getEmail()}</a></li>
+            <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.getGithub()}" target="_blank" rel="noopener noreferrer">${engineer.getGithub()}</a></li>
+        </ul>
+    </div>
+</div>
+    `
+    htmlTemplate.push(engineerCard)
+  }
+
+  function generateIntern(intern){
+   let internCard = `
+    <div class="card employee-card" style="width: 18rem;">
+    <div class="card-header">
+        <h2 class="card-title">${intern.getName()}</h2>
+        <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${intern.getRole()}</h3>
+    </div>
+    <div class="card-body">
+        <ul class="list-group">
+            <li class="list-group-item">ID:${intern.getId()}</li>
+            <li class="list-group-item">Email: <a href="${intern.getEmail()}">${intern.getEmail()}</a></li>
+            <li class="list-group-item">School: ${intern.getSchool()}</li>
+        </ul>
+    </div>
+</div>
+   `
+   htmlTemplate.push(internCard)
+  }
+
+  return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="./style.css">
+        <title>Employee Profile</title>
+        <script src="https://kit.fontawesome.com/de01171047.js" crossorigin="anonymous"></script>
+    </head>
+
+    <body>
+        <header>
+            <div class="jumbotron jumbotron-fluid">
+                <div class="container">
+                  <h1 class="display-4 text-center">My Team</h1>
+                </div>
+              </div>    
+        </header>
+          
+        <main>
+            ${htmlTemplate.join()};
+        </main>
+    </body>
+    </html>`
+};
+
+const generateHTML = (team) => {
+  fs.writeFile('./dist/team.html', employeesTemplate(team), () => {
+    console.log(team, "File written successfully!");
+  })
 }
+
+// , function(err, result) {
+//   (err) ? console.log('error', err): console.log(`Successfully created employee profile.`);
+//   }
+// module.exports = employeesTemplate;
+// module.exports = dataPassedIn => {return employeesTemplate(dataPassedIn)};
+module.exports = {generateHTML};
+
 // Step 1. export function to generate entire html page with template literal
 // Step 2. function to create team template
 // Step 3. functions to generate manager, engineer, and intern templates
 
 // Step 3a. function to generate manager card template
 //  - take in manager as input parameter, a manager object
-//  - create manager card template
+//  - create manager card template fict
 //  -   call get name, get role, get id, get email, and get office number methods to fill in the template
 //  - return the template to the caller
 
@@ -56,6 +178,3 @@ function renderHTML() {
 //  -   passing team , array of answer objects, as input argument
 //  - you will return the template to the caller
 //  - export the function
-
-
-module.exports = {renderHTML};
